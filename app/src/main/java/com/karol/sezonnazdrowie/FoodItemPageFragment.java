4da76a501c03dfccd5c4c089f0f12aeb3d4b541a
Karol Ksionek <karlkar@tlen.pl>
2016-03-26 12:09:43 +0100

    Fragments extracted to other classes

---
+++ app/src/main/java/com/karol/sezonnazdrowie/FoodItemPageFragment.java
@@ -0,0 +1,122 @@
+package com.karol.sezonnazdrowie;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+/**
+ * Created by Karol on 25.03.2016.
+ */
+public class FoodItemPageFragment extends Fragment {
+
+    private FoodItem mItem;
+
+    private ImageView mPagePreviewImageView;
+    private LinearLayout mAdditionalTextsLayout;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_food_item_page, null);
+        mItem = getArguments().getParcelable("ITEM");
+        ((ListActivity) getActivity()).setActionBarTitle(mItem.getName());
+
+        mPagePreviewImageView = (ImageView) view.findViewById(R.id.pagePreviewImageView);
+        if (!mItem.getImage().isEmpty())
+            mPagePreviewImageView.setImageResource(getResources().getIdentifier(mItem.getImage(), "drawable", getActivity().getPackageName()));
+        else
+            mPagePreviewImageView.setImageResource(android.R.drawable.ic_menu_gallery);
+
+        mAdditionalTextsLayout = (LinearLayout) view.findViewById(R.id.additionalTextsLayout);
+        TextView tmp = new TextView(getActivity());
+        tmp.setText(mItem.getDesc());
+        mAdditionalTextsLayout.addView(tmp);
+        addSpacer();
+        if (mItem.hasProximates()) {
+            // add wartość odżywcza
+            TextView title = new TextView(getActivity());
+            title.setText("Wartość odżywcza (100g)");
+            mAdditionalTextsLayout.addView(title);
+
+            addElementView("woda", mItem.getWater());
+            addElementView("kalorie", mItem.getEnergy());
+            addElementView("białka", mItem.getProtein());
+            addElementView("tłuszcze", mItem.getFat());
+            addElementView("węglowodany", mItem.getCarbohydrates());
+            addElementView("błonnik", mItem.getFiber());
+            addElementView("cukry", mItem.getSugars());
+            addSpacer();
+        }
+
+        if (mItem.hasMinerals()) {
+            // add zawartość minerałów
+            TextView title = new TextView(getActivity());
+            title.setText("Zawartość mikro- i makroelementów (100g)");
+            mAdditionalTextsLayout.addView(title);
+
+            addElementView("wapń", mItem.getCalcium());
+            addElementView("żelazo", mItem.getIron());
+            addElementView("magnez", mItem.getMagnesium());
+            addElementView("fosfor", mItem.getPhosphorus());
+            addElementView("potas", mItem.getPotassium());
+            addElementView("sód", mItem.getSodium());
+            addElementView("cynk", mItem.getZinc());
+            addSpacer();
+        }
+
+        if (mItem.hasVitamins()) {
+            // add zawartość witamin
+            TextView title = new TextView(getActivity());
+            title.setText("Witaminy (100g)");
+            mAdditionalTextsLayout.addView(title);
+
+            addElementView("witamina A", mItem.getVitA());
+            addElementView("witamina C", mItem.getVitC());
+            addElementView("witamina E", mItem.getVitE());
+            addElementView("witamina B1 (tiamina)", mItem.getThiamin());
+            addElementView("witamina B2 (ryboflawina)", mItem.getRiboflavin());
+            addElementView("witamina B3 (niacyna)", mItem.getNiacin());
+            addElementView("witamina B6", mItem.getVitB6());
+            addElementView("witamina K", mItem.getVitK());
+            addElementView("kwas foliowy", mItem.getFolate());
+            addSpacer();
+        }
+
+        if (mItem.hasProximates() || mItem.hasMinerals() || mItem.hasVitamins()) {
+            TextView title = new TextView(getActivity());
+            title.setText("Źródło: USDA National Nutrient Database");
+            mAdditionalTextsLayout.addView(title);
+            addSpacer();
+        }
+
+        AdView adView = (AdView) view.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(getString(R.string.adMobTestDeviceNote5))
+                .addTestDevice(getString(R.string.adMobTestDeviceS5))
+                .build();
+        adView.loadAd(adRequest);
+        return view;
+    }
+
+    private void addElementView(String title, String value) {
+        if (!value.isEmpty()) {
+            TextView tmp = new TextView(getActivity());
+            tmp.setText(title + " - " + value);
+            mAdditionalTextsLayout.addView(tmp);
+        }
+    }
+
+    private void addSpacer() {
+        TextView tmp = new TextView(getActivity());
+        mAdditionalTextsLayout.addView(tmp);
+    }
+}
---
+++ app/src/main/java/com/karol/sezonnazdrowie/ListFragment.java
@@ -0,0 +1,100 @@
+package com.karol.sezonnazdrowie;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Karol on 25.03.2016.
+ */
+public class ListFragment extends Fragment {
+
+    ListView mListView = null;
+    FoodItemAdapter mAdapter = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, null);
+
+        ArrayList<FoodItem> items = null;
+        String what = getArguments().getString("WHAT");
+        if (what.equals("FRUITS")) {
+            ((ListActivity)getActivity()).setActionBarTitle("SEZON NA OWOCE");
+            items = Database.getInstance().getCurrentFruits();
+//            items = Database.getInstance().getAllFruits();
+        } else if (what.equals("VEGETABLES")) {
+            ((ListActivity)getActivity()).setActionBarTitle("SEZON NA WARZYWA");
+            items = Database.getInstance().getCurrentVegetables();
+//            items = Database.getInstance().getAllVegetables();
+        }
+
+        mListView = (ListView) view.findViewById(R.id.listView);
+        mAdapter = new FoodItemAdapter(getActivity(), items);
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fragment fragment = new FoodItemPageFragment();
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("ITEM", (Parcelable) parent.getItemAtPosition(position));
+                fragment.setArguments(bundle);
+                ((ListActivity) getActivity()).replaceFragments(fragment);
+            }
+        });
+
+        AdView adView = (AdView) view.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(getString(R.string.adMobTestDeviceNote5))
+                .addTestDevice(getString(R.string.adMobTestDeviceS5))
+                .build();
+        adView.loadAd(adRequest);
+        return view;
+    }
+
+    private class FoodItemAdapter extends ArrayAdapter<FoodItem> {
+
+        private class ViewHolder {
+            TextView mText;
+        }
+
+        public FoodItemAdapter(Context context, ArrayList<FoodItem> objects) {
+            super(context, R.layout.row_layout, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.row_layout, parent, false);
+                viewHolder = new ViewHolder();
+                viewHolder.mText = (TextView) view.findViewById(R.id.rowText);
+                view.setTag(viewHolder);
+            } else {
+                view = convertView;
+                viewHolder = (ViewHolder) view.getTag();
+            }
+
+            FoodItem item = getItem(position);
+            viewHolder.mText.setText(item.getName());
+
+            return view;
+        }
+    }
+}
